1915f23272bc82b878fae5f568e22d0a37f157f9
Saman Mashayekhi <sami.sansilver10@gmail.com>
2023-12-02 11:33:27 +0330

    add Company 2 to git

--- resources/views/supply/results.blade.php
+++ resources/views/supply/results.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Sakht Tamin Konande') }}
+            {{ __('List Tamin Konandeha Ro Bebin') }}
         </h2>
     </x-slot>
     <div class="py-12" style="direction:rtl">
