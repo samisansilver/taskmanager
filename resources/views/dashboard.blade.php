01337ce9fd57fac744a4664ffacaddacc2748979
Saman Mashayekhi <sami.sansilver10@gmail.com>
2024-02-20 17:46:24 +0330

    edit last tasks in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>ایلیااستیل - تسک منیجر</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
