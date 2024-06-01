df0f6a1288d9069755b17a9cc293edec7d1fd132
root <sami.sansilver10@gmail.com>
2024-06-01 16:59:56 +0330

    edit command

--- app/Console/Commands/count_tasks_sms.php
+++ app/Console/Commands/count_tasks_sms.php
@@ -40,7 +40,7 @@ class count_tasks_sms extends Command
                     'message' => 'شما تعداد '.$findtasks.' تسک انجام نشده دارید',
                 ]);
 //                return $response->body();
-                Log::error($findtasks.''.$receptor);
+                Log::info($findtasks.''.$receptor);
             };
     }
 }
